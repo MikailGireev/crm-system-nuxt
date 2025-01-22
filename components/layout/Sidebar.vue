2ed82f0894e76915df170efb2dbb2ecfa832d600
Mikail <mikaildevelop@gmail.com>
2025-01-22 18:18:24 +0300

    Layout sidebar

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <aside class="px-5 bg-sidebar py-5 h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg src="/logo.svg" alt="logo" width="100px" class="mx-auto" />
+    </NuxtLink>
+
+    <UiButton>Logout</UiButton>
+  </aside>
+</template>
+
+<style scoped></style>
