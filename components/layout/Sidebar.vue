eb68c0c6e129edd89096f172e148c433a46def1a
Mikail <mikaildevelop@gmail.com>
2025-01-23 13:22:22 +0300

    Appwrite Backend

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -3,7 +3,7 @@
 <template>
   <aside class="px-5 bg-sidebar py-5 h-full relative w-full">
     <NuxtLink to="/" class="mb-10 block">
-      <NuxtImg src="/logo.svg" alt="logo" width="100px" class="mx-auto" />
+      <NuxtImg src="/logo.svg" alt="logo" width="140px" class="mx-auto" />
     </NuxtLink>
 
     <button class="absolute top-2 right-3 transition-colors hover:text-primary">
