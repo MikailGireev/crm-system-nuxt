9abb29ffe575d63432af0662d1d8d5895eec3a1c
Mikail <mikaildevelop@gmail.com>
2025-01-23 08:07:27 +0300

    Menu

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -6,8 +6,12 @@
       <NuxtImg src="/logo.svg" alt="logo" width="100px" class="mx-auto" />
     </NuxtLink>
 
-    <UiButton>Logout</UiButton>
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:logout" size="20" />
+    </button>
+    <LayoutMenu />
   </aside>
 </template>
 
 <style scoped></style>
+0
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+    '@nuxt/icon',
+  ],
   shadcn: {
     prefix: 'Ui',
     componentDir: './components/ui',
