e56920956c59b8850e6c1e1a6c44e1cf7766ee2f
Mikail <mikaildevelop@gmail.com>
2025-01-23 20:21:36 +0300

    Login/Register

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,4 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useAuthStore, useIsLoadingStore } from '@/store/auth.store';
+
+const isLoadingStore = useIsLoadingStore();
+const store = useAuthStore();
+const router = useRouter();
+
+const logout = async () => {
+  isLoadingStore.set(true);
+  await account.deleteSession('current');
+  store.clear();
+  await router.push('/login');
+  isLoadingStore.set(false);
+};
+</script>
 
 <template>
   <aside class="px-5 bg-sidebar py-5 h-full relative w-full">
@@ -6,7 +20,7 @@
       <NuxtImg src="/logo.svg" alt="logo" width="140px" class="mx-auto" />
     </NuxtLink>
 
-    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+    <button @click="logout" class="absolute top-2 right-3 transition-colors hover:text-primary">
       <Icon name="line-md:logout" size="20" />
     </button>
     <LayoutMenu />
--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { v4 as uuid } from 'uuid';
+
 useHead({
   title: 'Login | CRM System',
 });
@@ -8,8 +10,32 @@ const passwordRef = ref('');
 const nameRef = ref('');
 
 const isLoadingStore = useIsLoadingStore();
-
+const authStore = useAuthStore();
 const router = useRouter();
+
+const login = async () => {
+  isLoadingStore.set(true);
+  await account.createEmailPasswordSession(emailRef.value, passwordRef.value);
+  const response = await account.get();
+  if (response) {
+    authStore.set({
+      email: response.email,
+      name: response.name,
+      status: response.status,
+    });
+  }
+  emailRef.value = '';
+  passwordRef.value = '';
+  nameRef.value = '';
+
+  await router.push('/');
+  isLoadingStore.set(false);
+};
+
+const register = async () => {
+  await account.create(uuid(), emailRef.value, passwordRef.value, nameRef.value);
+  await login();
+};
 </script>
 
 <template>
@@ -21,8 +47,8 @@ const router = useRouter();
         <UiInput v-model="passwordRef" placeholder="Password" type="password" class="mb-3" />
         <UiInput v-model="nameRef" placeholder="Name" type="name" class="mb-3" />
         <div class="flex items-center justify-center gap-5">
-          <UiButton type="button">Login</UiButton>
-          <UiButton type="button">Register</UiButton>
+          <UiButton type="button" @click="login">Login</UiButton>
+          <UiButton type="button" @click="register">Register</UiButton>
         </div>
       </form>
     </div>
