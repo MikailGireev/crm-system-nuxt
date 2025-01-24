04a977e6007267a7e45df384dbda944cf4b760bd
Mikail <mikaildevelop@gmail.com>
2025-01-24 22:53:08 +0300

    Kanban

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,7 @@ export default defineNuxtConfig({
     ],
     '@nuxt/icon',
     '@pinia/nuxt',
+    ['@vee-validate/nuxt', { autoImports: true }],
   ],
   shadcn: {
     prefix: 'Ui',
--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,7 @@ const { data, isLoading, refetch } = useKanbanQuery();
         <div v-for="(column, index) in data" :key="index">
           <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">{{ column.name }}</div>
           <div>
+            <KanbanCreateDeal :refetch="refetch" :status="column.id" />
             <UiCard v-for="card in column.cards" :key="card.id" class="mb-3" draggable="true">
               <UiCardHeader role="button">
                 <UiCardTitle>{{ card.name }}</UiCardTitle>
